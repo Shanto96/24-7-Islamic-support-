619d9d14c8d47d988f652c1f8b2792188bf1aebc
Shanto96 <32538444+Shanto96@users.noreply.github.com>
2020-11-05 02:04:12 +0600

    updating views

--- aya/askyouralem/aya/views.py
+++ aya/askyouralem/aya/views.py
@@ -3,12 +3,26 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import CreateUserForm
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 
 def loginpage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username, password = password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'Username or password did not mathced')
+
     context = {}
     return render(request,'aya/login.html')
+
 def registration(request):
 
     form = CreateUserForm()
