f80d3bc9bca2e6b0d4abdda5729f8315e80b6cbd
Shanto96 <32538444+Shanto96@users.noreply.github.com>
2020-11-05 01:05:57 +0600

    Update views.py

--- aya/askyouralem/aya/views.py
+++ aya/askyouralem/aya/views.py
@@ -1,10 +1,28 @@
-
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
+from .forms import CreateUserForm
+from django.contrib import messages
 # Create your views here.
 
-def login(request):
+
+def loginpage(request):
     context = {}
     return render(request,'aya/login.html')
-#fgljgfld;skjgdslfjgsldfjg sl;gkjmsdflmggdlf;mbgdlfmldskgjsd;lfg
+def registration(request):
+
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for '+ )
+            return redirect('login')
+
+
+    context = {'form':form}
+    return render(request,'aya/register.html',context)
+def home(request):
+    context = {}
+    return render(request,'aya/home.html')
