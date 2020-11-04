67a7e4b4765c26e6f205eb75ec5cb1a40dcbb58a
Shanto96 <32538444+Shanto96@users.noreply.github.com>
2020-11-05 01:11:07 +0600

    urls.py

--- aya/askyouralem/aya/urls.py
+++ aya/askyouralem/aya/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = {
-    path('login',views.login,name="login")
-}
+urlpatterns = [
+    path('login/',views.loginpage,name="login"),
+    path('registration/',views.registration,name="registration"),
+    path('home/',views.home,name="home"),
+]
