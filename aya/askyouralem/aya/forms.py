af17f7d880ad95a69f2abd138c6190a3a9255687
Shanto96 <32538444+Shanto96@users.noreply.github.com>
2020-11-23 22:58:22 +0600

    user creation 23
    sdafsd

--- aya/askyouralem/aya/forms.py
+++ aya/askyouralem/aya/forms.py
@@ -6,9 +6,12 @@ from django import forms
 
 
 class CreateUserForm(UserCreationForm):
+    first_name = forms.CharField(max_length=32, help_text='First name')
+    last_name = forms.CharField(max_length=32, help_text='Last name')
+
     class Meta:
         model  =  User
-        fields = ['username','email','password1','password2']
+        fields = ['username','email','password1','password2','first_name','last_name','email']
 
 
 class QuestionForm(ModelForm):
